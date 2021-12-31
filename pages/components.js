b7f77a01b33340a5ddca806f74a1eedd7f2e321e
Sarthak Verma <sarthakvdev@gmail.com>
2021-12-31 20:02:53 +0530

    Fixes

---
+++ pages/components.js
@@ -0,0 +1,24 @@
+export const Para = ({ desc }) => (
+  <p
+    style={{
+      fontSize: '0.9em',
+      color: 'gray',
+      marginTop: '20px',
+      marginBottom: '5px',
+    }}
+  >
+    {desc}
+  </p>
+);
+
+export const ProjTimeline = ({ desc }) => (
+  <p
+    style={{
+      fontSize: "0.9em",
+      color: "gray",
+      margin: "0"
+    }}
+  >
+    {desc}
+  </p>
+);
